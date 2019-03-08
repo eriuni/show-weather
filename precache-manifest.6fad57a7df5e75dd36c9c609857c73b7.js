02e3bdcb384a8eb2ffb6b942a717119048ac78fe
Erion Muca <eriuni@gmail.com>
2019-03-08 22:02:07 +0100

    Updates

--- precache-manifest.6fad57a7df5e75dd36c9c609857c73b7.js
+++ precache-manifest.6fad57a7df5e75dd36c9c609857c73b7.js
@@ -20,12 +20,12 @@ self.__precacheManifest = [
     "url": "/show-weather/static/js/2.4afe843a.chunk.js"
   },
   {
-    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
-    "url": "/show-weather/static/media/outline-icons.ef60a4f6.woff"
-  },
-  {
     "revision": "cd6c777f1945164224dee082abaea03a",
     "url": "/show-weather/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/show-weather/static/media/outline-icons.ef60a4f6.woff"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
@@ -56,24 +56,24 @@ self.__precacheManifest = [
     "url": "/show-weather/static/media/brand-icons.a046592b.woff"
   },
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/show-weather/static/media/icons.b87b9ba5.ttf"
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/show-weather/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
-    "url": "/show-weather/static/media/brand-icons.c5ebe0b3.ttf"
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/show-weather/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/show-weather/static/media/brand-icons.13db00b7.eot"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/show-weather/static/media/outline-icons.82f60bd0.svg"
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/show-weather/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/show-weather/static/media/icons.8e3c7f55.eot"
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/show-weather/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "962a1bf31c081691065fe333d9fa8105",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/show-weather/precache-manifest.4dcb9bafebf711e659e513f4d3487239.js"
+  "/show-weather/precache-manifest.6fad57a7df5e75dd36c9c609857c73b7.js"
 );
 
 workbox.clientsClaim();
